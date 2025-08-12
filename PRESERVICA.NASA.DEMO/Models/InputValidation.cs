e86df2fa058f72b4325cb26920956ab8248f9e18
Pavan Duvvuri <pavanready@gmail.com>
2025-08-12 23:34:54 +0100

    playwright

---
+++ PRESERVICA.NASA.DEMO/Models/InputValidation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRESERVICA.NASA.DEMO.Models
+{
+    public class InputValidation
+    {
+        public string FieldName { get; set; }
+        public string ErrorMessage { get; set; }
+        public string InputScenario { get; set; }
+    }
+}
